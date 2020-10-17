428537ed35a3f3bda6c78d0e0f88c185e6b280a3
liuyl <mqkfc123@163.com>
2020-10-17 14:23:40 +0800

    test

--- src/SkyApm.Transport.Http/V6/PingCaller.cs
+++ src/SkyApm.Transport.Http/V6/PingCaller.cs
@@ -40,7 +40,7 @@ namespace SkyApm.Transport.Grpc.V6
             param.Add("InstancePingPkg", serviceInstancePingPkg);
 
             //http 请求
-            var result = HttpHelper.PostMode(_config.Servers + heartbeat, Newtonsoft.Json.JsonConvert.SerializeObject(param));
+            var result = HttpHelper.PostMode(_config.Servers + heartbeat, Newtonsoft.Json.JsonConvert.SerializeObject(serviceInstancePingPkg));
             if (string.IsNullOrEmpty(result))
             {
               
